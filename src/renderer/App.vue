b8aa11a9e674b5a0fcf9aefd47cb4a8b76676364
Daniel Hart <daniel.hart@juicymedia.co.uk>
2023-10-17 08:11:14 +0000

    Resolve "Set up Electron"

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,27 +1,11 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="ukpid-logo">UK Poisons Information Database</div>
+
+  <h1>Welcome</h1>
+
+  <UkpidButton appearance="accent">Hello!</UkpidButton>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import UkpidButton from "./components/UkpidButton.vue";
+</script>
---
+++ src/renderer/components/UkpidButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <fluent-button :appearance="props.appearance">
+    <slot />
+  </fluent-button>
+</template>
+
+<script setup>
+const props = defineProps({
+  appearance: {
+    type: String,
+    default: "accent",
+    validator: (value) =>
+      ["accent", "lightweight", "neutral", "outline", "stealth"].includes(
+        value,
+      ),
+  },
+});
+</script>
