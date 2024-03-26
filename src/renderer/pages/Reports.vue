5dfd565b201a9014b74cfed527364ed643ddf87d
Daniel Hart <daniel.hart@juicymedia.co.uk>
2024-03-26 10:45:23 +0000

    WIP: Reports

--- src/renderer/pages/Reports.vue
+++ src/renderer/pages/Reports.vue
@@ -1,13 +1,24 @@
 <template>
-  <MainWindowPage>
-    <h1>{{ $t("Reports") }}</h1>
+  <MainWindowPage class="ReportPage">
+    <iframe class="ReportFrame" :src="reportUrl"> </iframe>
   </MainWindowPage>
 </template>
 
 <script setup>
 import MainWindowPage from "@/components/MainWindowPage.vue";
+
+const reportUrl = import.meta.env.UKPID_REPORT_URL;
 </script>
 
 <style lang="scss" scoped>
-// tbc
+.ReportPage {
+  padding: 0;
+}
+
+.ReportFrame {
+  display: block;
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
 </style>
